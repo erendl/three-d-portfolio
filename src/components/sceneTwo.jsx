97d58f9a0c70dacf3221ef88e60f26dd0bb8d62c
erendl <erenozdil11@gmail.com>
2025-07-08 20:19:14 +0300

    fix

--- src/components/sceneTwo.jsx
+++ src/components/sceneTwo.jsx
@@ -171,6 +171,9 @@ function SceneTwo({ setLoading, onContactClick }) {
         camera.rotation.set(-2.970708832007452, -0.006149622266331087, -3.1405314398888207);
         controls.target.set(-0.5233570373593516, 0.5518531521781637, 0.09849760556329873);
         controls.rotateSpeed = 0.3;
+        if (window.innerWidth <= 768) {
+          camera.position.set(-0.5256585328246953, 0.6154948028987944, -1.5);
+        }
 
 
         function animate() {
